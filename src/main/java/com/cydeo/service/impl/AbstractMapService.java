a3144bb3e1557f34bb4332498e3b8b31f9a2b648
violettaqueen <violetta.v.queen@gmail.com>
2022-09-30 17:24:23 -0600

    Abstract Map Methods created

---
+++ src/main/java/com/cydeo/service/impl/AbstractMapService.java
@@ -0,0 +1,30 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.service.CrudService;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public abstract class AbstractMapService <T, ID> {
+
+    //create DB
+
+    public Map<ID,T> map = new HashMap<>();
+
+    T save(ID id, T object){
+        map.put(id, object);
+        return object;
+    }
+    T findById(ID id){
+       return map.get(id);
+    }
+    List<T> findAll(){
+        return new ArrayList<>(map.values());
+    }
+    void deleteById(ID id){
+        map.remove(id);
+    }
+
+}
