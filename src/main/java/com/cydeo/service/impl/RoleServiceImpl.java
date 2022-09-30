ad804463b2d0694d01ee3a368a0eba6f118a1ff3
violettaqueen <violetta.v.queen@gmail.com>
2022-09-30 17:35:04 -0600

    RoleServiceImpl implemented RoleService and Extended AbstractMapService

--- src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -5,24 +5,24 @@ import com.cydeo.service.RoleService;
 
 import java.util.List;
 
-public class RoleServiceImpl implements RoleService {
+public class RoleServiceImpl extends AbstractMapService<RoleDTO, Long> implements RoleService {
     @Override
-    public RoleDTO save(RoleDTO user) {
-        return null;
+    public RoleDTO save(RoleDTO role) {
+        return super.save(role.getId(), role);
     }
-
     @Override
-    public RoleDTO findById(Long username) {
-        return null;
+    public RoleDTO findById(Long id) {
+        return super.findById(id);
     }
 
     @Override
     public List<RoleDTO> findAll() {
-        return null;
+        return super.findAll();
     }
 
     @Override
-    public void deleteById(Long username) {
+    public void deleteById(Long id) {
+        super.deleteById(id);
 
     }
 }
