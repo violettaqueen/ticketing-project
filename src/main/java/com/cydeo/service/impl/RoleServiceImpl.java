3658a02fdbb21474716af3719dbabffc67b148b2
violettaqueen <violetta.v.queen@gmail.com>
2022-09-30 16:44:47 -0600

    Service classes created

---
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -0,0 +1,28 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.RoleDTO;
+import com.cydeo.service.RoleService;
+
+import java.util.List;
+
+public class RoleServiceImpl implements RoleService {
+    @Override
+    public RoleDTO save(RoleDTO user) {
+        return null;
+    }
+
+    @Override
+    public RoleDTO findById(Long username) {
+        return null;
+    }
+
+    @Override
+    public List<RoleDTO> findAll() {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Long username) {
+
+    }
+}
