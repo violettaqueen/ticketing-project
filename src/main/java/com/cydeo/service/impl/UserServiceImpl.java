34423eb21e2c64724ca38579fed04bee7fb8ff9b
violettaqueen <violetta.v.queen@gmail.com>
2022-09-30 21:23:25 -0600

    UserServiceImpl completed

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -1,4 +1,29 @@
 package com.cydeo.service.impl;
 
-public class UserServiceImpl {
+import com.cydeo.dto.UserDTO;
+import com.cydeo.service.UserService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserServiceImpl extends AbstractMapService<UserDTO, String> implements UserService {
+    @Override
+    public UserDTO save(UserDTO user) {
+        return super.save(user.getUserName(), user);
+    }
+
+    @Override
+    public UserDTO findById(String username) {
+        return super.findById(username);
+    }
+
+    @Override
+    public List<UserDTO> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(String username) {
+        super.deleteById(username);
+    }
 }
