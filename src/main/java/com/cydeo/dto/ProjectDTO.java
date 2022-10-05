afd7b899ac484ee66b53f4f29b1021a23cd2059e
violettaqueen <violetta.v.queen@gmail.com>
2022-10-05 14:08:07 -0600

    ProjectDTO getters added

--- src/main/java/com/cydeo/dto/ProjectDTO.java
+++ src/main/java/com/cydeo/dto/ProjectDTO.java
@@ -1,16 +1,30 @@
 package com.cydeo.dto;
 
 import com.cydeo.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProjectDTO {
 
     private String projectName;
+
     private String projectCode;
+
     private UserDTO assignedManager;
+
+    @DateTimeFormat(pattern = "MM-dd-yyyy")
     private LocalDate projectStartDate;
+
+    @DateTimeFormat(pattern = "MM-dd-yyyy")
     private LocalDate projectEndDate;
+
     private String projectDetail;
+
     private Status projectStatus;
 }
