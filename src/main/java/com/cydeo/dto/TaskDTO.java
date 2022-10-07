c029b12b3d2422266721205a63be0680692b3143
violettaqueen <violetta.v.queen@gmail.com>
2022-10-06 21:10:44 -0600

    Work on Task object has begun: button click activated, Controller created, TaskDTO created

--- src/main/java/com/cydeo/dto/TaskDTO.java
+++ src/main/java/com/cydeo/dto/TaskDTO.java
@@ -12,6 +12,7 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class TaskDTO {
 
+    private Long id;
     private ProjectDTO project;
     private UserDTO assignedEmployee;
     private String taskSubject;
