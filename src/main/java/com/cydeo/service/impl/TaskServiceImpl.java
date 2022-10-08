aecc01cac0d91fd2696b94df1999fc339819feee
violettaqueen <violetta.v.queen@gmail.com>
2022-10-08 13:46:48 -0600

    findTasksByManager Service is created and implemented

--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -1,6 +1,7 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.TaskDTO;
+import com.cydeo.dto.UserDTO;
 import com.cydeo.enums.Status;
 import com.cydeo.service.TaskService;
 import org.springframework.stereotype.Service;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class TaskServiceImpl extends AbstractMapService<TaskDTO, Long> implements TaskService {
@@ -52,6 +54,16 @@ public class TaskServiceImpl extends AbstractMapService<TaskDTO, Long> implement
     @Override
     public void deleteById(Long id) {
         super.deleteById(id);
+
+    }
+
+    @Override
+    public List<TaskDTO> findTasksByManager(UserDTO manager) {
+
+        return  findAll()
+                        .stream()
+                        .filter(task -> task.getProject().getAssignedManager().equals(manager))
+                        .collect(Collectors.toList());
 
     }
 }
